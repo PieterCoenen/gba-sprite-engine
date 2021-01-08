d41caf1ae3344817f36ca4431c7719f0a030b214
Pieter Coenen <r0668811@ucll.be>
2021-01-08 17:51:52 +0100

    updated the CMakelist

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,9 +1,5 @@
 #include <libgba-sprite-engine/scene.h>
 #include <libgba-sprite-engine/gba_engine.h>
-#include <libgba-sprite-engine/background/text.h>
-#include <libgba-sprite-engine/background/text_stream.h>
-#include <libgba-sprite-engine/palette/palette_manager.h>
-#include <libgba-sprite-engine/allocator.h>
 
 #include "pod_scene.h"
 #include "SplashScreen.h"
