4502966f265c6c64874d838ac6d07b1e1d850827
S. Arne <Arne_schepers@hotmail.com>
2021-01-26 16:28:51 +0100

    Palled manager toegevoegd

--- Project/src/SplashScreen.cpp
+++ Project/src/SplashScreen.cpp
@@ -27,7 +27,7 @@ void SplashScreen::tick(u16 keys) {
 void SplashScreen::load() {
     engine.get()->disableText();
 
-    //backgroundPalette = std::unique_ptr<BackgroundPaletteManager>(new BackgroundPaletteManager(splashimagePal, sizeof(splashimagePal)));
+    backgroundPalette = std::unique_ptr<BackgroundPaletteManager>(new BackgroundPaletteManager(Space1Pal, sizeof(Space1Pal)));
 
     bg = std::unique_ptr<Background>(new Background(1, Space1Tiles, sizeof(Space1Tiles), Space1Map, sizeof(Space1Map)));
     bg.get()->useMapScreenBlock(24);
